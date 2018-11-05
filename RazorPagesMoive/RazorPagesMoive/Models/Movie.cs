67c7058dee80a4c084a9a5d73649ba8e2273fc65
lizhang <zhanglitianjingmei@163.com>
2018-11-06 00:31:50 +0800

    add Movie  to test

---
+++ RazorPagesMoive/RazorPagesMoive/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorPagesMoive.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public String Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public String Genre { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
